8ec225d3dbed84c2c915c81fdbf0252efb6ecf2a
Sebastian <cph-sb287@cphbusiness.dk>
2020-09-15 12:49:01 +0200

    reload button added

--- src/main/webapp/scripts/indexScript.js
+++ src/main/webapp/scripts/indexScript.js
@@ -4,22 +4,28 @@ function getTableHeader() {
 }
 
 function getTableRow(grpMember) {
-    return `<tr><th scope="row">${grpMember.name}</th><td>${grpMember.studentID}</td><td>${grpMember.favoriteTVSeries}</td></tr>`;
+    return `<tr><td scope="row">${grpMember.name}</td><td>${grpMember.studentID}</td><td>${grpMember.favoriteTVSeries}</td></tr>`;
 }
 
 
- function fetchAllMovies() {
-                let url = "https://svendbentsballonshow.dk/ca1/api/groupmembers/all";
-                fetch(url)
-                        .then(res => res.json()) //in flow1, just do it
-                        .then(data => {
-                            let memberTable = document.getElementById("memberTable");
-                            let result = "";
-                            data.forEach((x) => {
-                                result += getTableRow(x)
-                            });
-                            memberTable.innerHTML = `<table class="table">${getTableHeader()}${result}</tbody></table>`;
-                        });
-            }
-            
-document.getElementById("memberTable").innerHTML = fetchAllMovies();
+function fetchAllMembers() {
+    let url = "https://svendbentsballonshow.dk/ca1/api/groupmembers/all";
+    fetch(url)
+            .then(res => res.json()) //in flow1, just do it
+            .then(data => {
+                let memberTable = document.getElementById("memberTable");
+                let result = "";
+                data.forEach((x) => {
+                    result += getTableRow(x)
+                });
+                memberTable.innerHTML = `<table class="table">${getTableHeader()}${result}</tbody></table>`;
+            });
+}
+
+fetchAllMembers();
+document.getElementById("reloadBtn").addEventListener("click", function(e){
+    e.preventDefault();
+    fetchAllMembers();
+});
+
+
